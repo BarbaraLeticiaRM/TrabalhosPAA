ef1ea404c37a57feb71c69a14613e45b88207a42
BarbaraLMilagres <58093095+BarbaraLMilagres@users.noreply.github.com>
2023-04-24 20:20:28 -0300

    Alterações no algoritmo de interpolação

--- Diminuir e Conquistar/buscainterpolacao.cpp
+++ Diminuir e Conquistar/buscainterpolacao.cpp
@@ -2,52 +2,37 @@
 using namespace std;
 #define TAM 8
 
-int interpolationSearch(int A[], int n, int y)
+int interpolationSearch(int A[], int n, int y, int I, int F)
 {
-    // I - início do vetor
-    // F - fim do vetor
-    // x - meio 
-    // y - elemento a ser buscado
-    // n - tamanho do vetor
-
     if (n == 0) {
         return -1;
     }
 
-    int I = 0, F = n - 1, x;
- 
-    while (A[F] != A[I] && y >= A[I] && y <= A[F])
-    {
-        x = I + ((y - A[I]) * (F - I) / (A[F] - A[I]));
- 
-        if (y == A[x]) {
-            return x;
-        }
-        
-        else if (y < A[x]) {
-            F = x - 1;
-        }
-        
-        else {
-            I = x + 1;
-        }
-    }
- 
-    if (y == A[I]) {
-        return I;
-    }
- 
-    else {
+    if (A[F] == A[I] || y < A[I] || y > A[F]) {
         return -1;
+    }
+
+    int x = I + ((y - A[I]) * (F - I) / (A[F] - A[I]));
+ 
+    if (y == A[x]) {
+        return x;
+    }
+        
+    else if (y < A[x]) {
+        return interpolationSearch(A, n, y, I, x - 1);
+    }
+        
+    else {
+        return interpolationSearch(A, n, y, x + 1, F);
     }
 }
  
 int main(void)
 {
     int A[TAM] = {2, 5, 6, 8, 9, 10, 12, 17};
-    int y =12;
+    int y =10;
  
-    cout << "Posição: " << interpolationSearch(A, TAM, y) << endl;
+    cout << "Posição: " << interpolationSearch(A, TAM, y, 0, TAM - 1) << endl;
  
     return 0;
 }
