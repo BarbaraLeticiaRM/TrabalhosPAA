1d92bb1c490850f4ef36e702fae1f8a38e48dd9a
BarbaraLMilagres <58093095+BarbaraLMilagres@users.noreply.github.com>
2023-05-08 19:49:21 -0300

    Ajustes no quicksort

--- Dividir e Conquistar/quicksort.cpp
+++ Dividir e Conquistar/quicksort.cpp
@@ -1,28 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define TAM 6
+#define TAM 12
 
 int partition(int arr[], int l, int r)
 {
 	int pivot = arr[r];
 
-	int i = (l - 1);
+	int i = l;
 
 	for (int j = l; j <= r - 1; j++) {
 		if (arr[j] < pivot) {
-			i++;
 			swap(arr[i], arr[j]);
+			i++;
 		}
 	}
-	swap(arr[i + 1], arr[r]);
-	return (i + 1);
+	swap(arr[i], arr[r]);
+	return i;
 }
 
 void quickSort(int arr[], int l, int r)
 {
 	if (l < r) {
 		int p = partition(arr, l, r);
-
 		quickSort(arr, l, p - 1);
 		quickSort(arr, p + 1, r);
 	}
@@ -30,11 +29,12 @@ void quickSort(int arr[], int l, int r)
 
 int main()
 {
-	int arr[TAM] = { 10, 7, 8, 9, 1, 5 };
+	int arr[TAM] = { 10, 7, 8, 9, 1, 5, 9, 17, 19, 25, 38, 42 };
 
 	quickSort(arr, 0, TAM - 1);
 	cout << "Array ordenado: " << endl;
 	for (int i = 0; i < TAM; i++)
 		cout << arr[i] << " ";
+	cout << endl;
 	return 0;
 }
